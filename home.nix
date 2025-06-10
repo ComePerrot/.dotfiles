1760acb06b398bf6ea7f51157b25d12892f5df8e
Come Perrot <cperrot@asosan>
2025-06-10 09:24:26 +0200

    first config

---
+++ home.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+{
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+  };
+
+  home = {
+    stateVersion = "24.11";
+
+    username = lib.mkDefault "cperrot"; 
+    homeDirectory = lib.mkDefault "/home/cperrot";
+
+    packages = with pkgs; [
+      # Other utils
+      ncdu
+      tlrc
+      wget
+      element-desktop
+      wezterm
+    ]; 
+  };
+
+  fonts = {
+    fontconfig.enable = true;
+  };
+}
