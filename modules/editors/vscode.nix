b5f533d09b0a8e250e5581b607306269ae35a462
Côme Perrot <cperrot@laas.fr>
2025-09-16 13:36:20 +0200

    Add vscode settings

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -1,13 +1,25 @@
 {
   pkgs,
+  config,
   ...
 }: {
   programs.vscode = {
   enable = true;
   profiles.default.extensions = with pkgs.nix-vscode-extensions.open-vsx; [
-    continue.continue
+    # Utils
+    # continue.continue
     eamodio.gitlens
     oderwat.indent-rainbow
+    # Spell check
+    ltex-plus.vscode-ltex-plus
+    # Python
+    ms-python.python
+    ms-python.debugpy
+    charliermarsh.ruff
+    # Nix
+    jnoortheen.nix-ide
   ];
 };
+
+  xdg.configFile."Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/vscode_settings.json";
 }
