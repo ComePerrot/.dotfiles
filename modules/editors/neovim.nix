eb5e2b3b94e61fc38172bbfe1c104a8e9df012af
Côme Perrot <cperrot@laas.fr>
2025-09-16 16:23:09 +0200

    Update vim config following nvf example

--- modules/editors/neovim.nix
+++ modules/editors/neovim.nix
@@ -1,6 +1,5 @@
-{inputs, ...}:
-{
-  imports = [ 
+{inputs, ...}: {
+  imports = [
     inputs.nvf.homeManagerModules.default
   ];
 
@@ -18,18 +17,76 @@
         languages = ["en" "fr"];
       };
 
-      lsp.enable = true;
+      lsp = {
+        enable = true;
+
+        formatOnSave = false;
+        lspkind.enable = false;
+        lightbulb.enable = false;
+        trouble.enable = false;
+        nvim-docs-view.enable = true;
+      };
+
       languages = {
+        enableFormat = true;
         enableTreesitter = true;
+        enableExtraDiagnostics = true;
 
         nix = {
           enable = true;
-          format.enable = true;
           format.type = "alejandra";
-
           lsp.server = "nixd";
         };
+        markdown.enable = true;
         python.enable = true;
+        typst.enable = true;
+      };
+
+      visuals = {
+        nvim-scrollbar.enable = true;
+        nvim-web-devicons.enable = true;
+        cinnamon-nvim.enable = false;
+
+        highlight-undo.enable = true;
+        indent-blankline.enable = true;
+      };
+
+      statusline = {
+        lualine = {
+          enable = true;
+        };
+      };
+
+      autopairs.nvim-autopairs.enable = true;
+
+      autocomplete.nvim-cmp.enable = true;
+
+      filetree = {
+        neo-tree = {
+          enable = true;
+        };
+      };
+
+      binds = {
+        whichKey.enable = true;
+        cheatsheet.enable = true;
+      };
+
+      utility.motion = {
+        precognition.enable = true;
+      };
+
+      telescope.enable = true;
+      git = {
+        enable = true;
+        gitsigns.enable = true;
+        gitsigns.codeActions.enable = false; # throws an annoying debug message
+        neogit.enable = true;
+      };
+
+      ui.noice.enable = true;
+      comments = {
+        comment-nvim.enable = true;
       };
     };
   };
