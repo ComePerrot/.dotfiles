73e87fcf4818eb7da1d58435e3dd9020200303dc
Côme Perrot <cperrot@laas.fr>
2025-10-19 19:34:58 +0200

    Add cyberdream theme to stylix
    https://github.com/scottmckendry/cyberdream.nvim

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,14 +3,24 @@
     enable = true;
     autoEnable =false;
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/outrun-dark.yaml";
+    base16Scheme = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream.yaml";
+      sha256 = "1bfi479g7v5cz41d2s0lbjlqmfzaah68cj1065zzsqksx3n63znf";
+    };
+
+    # Light version
+    # base16Scheme = builtins.fetchurl {
+    #   url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream-light.yaml";
+    #   sha256 = "07fpphdx396wi7093dqyhvpmacnradi7xqxxcwp89x9pnwqxjvq3";
+    # };
+
     image = ../assets/background_dark.jpg;
     polarity = "dark";
 
-    # targets = {
-    #   gnome.enable = true;
-    #   gtk.enable = true;
-    #   qt.enable = true;
-    # };
+    targets = {
+      gnome.enable = true;
+      gtk.enable = true;
+      qt.enable = true;
+    };
   };
 }
