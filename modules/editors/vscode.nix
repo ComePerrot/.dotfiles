84d16fdf83c05aaccb1362e183b38fb963aeff9d
Côme Perrot <cperrot@laas.fr>
2025-12-16 17:35:47 +0100

    update vscode extensions

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -4,23 +4,24 @@
   ...
 }: {
   programs.vscode = {
-  enable = true;
-  profiles.default.extensions = with pkgs.nix-vscode-extensions.open-vsx; [
-    # Utils
-    # continue.continue
-    eamodio.gitlens
-    oderwat.indent-rainbow
-    # Spell check
-    ltex-plus.vscode-ltex-plus
-    # Python
-    ms-python.python
-    ms-python.debugpy
-    charliermarsh.ruff
-    # Nix
-    jnoortheen.nix-ide
-    # Toml
-    tamasfe.even-better-toml
-  ];
+    enable = true;
+    profiles.default.extensions = with pkgs.vscode-extensions; [
+      # AI assistant
+      continue.continue # Apache License 2.0
+      # Utils
+      eamodio.gitlens # MIT License for base files
+      oderwat.indent-rainbow # MIT License
+      # Spell check
+      ltex-plus.vscode-ltex-plus # Mozilla Public License 2.0
+      # Python
+      ms-python.python # MIT License
+      ms-python.debugpy # MIT License
+      # Nix
+      jnoortheen.nix-ide # MIT License
+      # Toml
+      tamasfe.even-better-toml # MIT License
+    ];
+    # profiles.default.extensions = with pkgs.nix-vscode-extensions.open-vsx; [];
 };
 
   xdg.configFile."Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/vscode_settings.json";
